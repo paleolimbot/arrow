9a3be69156c5fa0835a93a257255352cbf84e2db
Dewey Dunnington <dewey@wherobots.com>
2025-02-06 23:31:13 -0600

    maybe use the right header

--- cpp/src/parquet/schema.cc
+++ cpp/src/parquet/schema.cc
@@ -28,7 +28,7 @@
 #include "parquet/exception.h"
 #include "parquet/schema_internal.h"
 #include "parquet/thrift_internal.h"
-#include "parquet/windows_compatibility.h"
+#include "parquet/windows_fixup.h"
 
 using parquet::format::SchemaElement;
 
